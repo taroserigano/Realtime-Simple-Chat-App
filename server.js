34ee3cc3ba88f0af084eec1074e021e4456061b0
Taro Serigano <taroserigano@gmail.com>
2022-08-19 11:36:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const io = require('socket.io')(3000)
 
 const users = {}
 
+# server generates process
+
 io.on('connection', socket => {
+  
+  # when user enter name in prompt message window, script emit "new-user" signal 
+  # server socket.on accepts it, and drop "name"
+  # then emit out "user-connected" signal, which script again process it, and append the message
+  
   socket.on('new-user', name => {
     users[socket.id] = name
     socket.broadcast.emit('user-connected', name)
